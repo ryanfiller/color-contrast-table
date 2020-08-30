63a02a2c499fdef1f89764a904502bfce615e4ad
Ryan Filler <ryanfiller89@gmail.com>
2020-08-29 21:25:37 -0500

    remove comments

--- react/src/chart.jsx
+++ react/src/chart.jsx
@@ -4,47 +4,6 @@ import generateChart from 'color-contrast-table'
 import Table from './table.jsx'
 import Styles from './styles.jsx'
 
-// class Chart extends React.Component {
-//   constructor(props) {
-//     super(props)
-//     this.state = {
-//       colors: props.colors
-//     }
-//   }
-
-//   onNamesChange = (index, value) => {
-//     // guard against array map failing
-//     if (value === '') { value = `Color ${index + 1}` }
-//     const newColors = [...this.state.colors]
-//     newColors[index].name = value
-//     this.setState({colors: newColors})
-//   }
-
-//   onValuesChange = (index, value) => {
-//     const newColors = [...this.state.colors]
-//     newColors[index].value = value
-//     this.setState({colors: newColors})
-//   }
-
-//   render() {
-//     const colors = generateChart(this.state.colors)
-
-//     return (
-//       <React.Fragment>
-//         {this.props.useStyles && <Styles />}
-//         <Table
-//           colors={colors}
-//           editNames={!!this.props.editNames || !!this.props.onNamesChange}
-//           editValues={!!this.props.editValues || !!this.props.onValuesChange}
-//           onNamesChange={this.props.onNamesChange || this.onNamesChange}
-//           onValuesChange={this.props.onValuesChange || this.onValuesChange}
-//           onInputBlur={this.props.onInputBlur}
-//         />
-//       </React.Fragment>
-//     )
-//   }
-// }
-
 const Chart = props => {
 
   const [colors, setColors] = useState(props.colors)
